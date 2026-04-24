010037875ca50ccbab553a5116c787778c053709
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-04-24 14:02:44 +0530

    latest changes

--- 2-eks-cluster/backend.tf
+++ 2-eks-cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dev-eks-cluster-tf-state"
-    key = "laxman-eks-cluster/2-eks-cluster/terraform.tfstate"
+    key = "laxman-eks-cluster/1-network/terraform.tfstate"
     region = "us-east-2"
     dynamodb_table = "dev-eks-cluster-table-tf-lock"
     encrypt = true
