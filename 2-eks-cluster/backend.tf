04b49bdf06758b20926832cb4f05909cf40447b4
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-04-24 14:08:04 +0530

    latest changes

--- 2-eks-cluster/backend.tf
+++ 2-eks-cluster/backend.tf
@@ -3,8 +3,8 @@ data "terraform_remote_state" "network" {
 
   config = {
     bucket         = "dev-eks-cluster-tf-state"   # S3 bucket where network state is stored
-    key            = "1-network/terraform.tfstate" # path inside the bucket
-    region         = "ap-south-1"                 # AWS region
+    key            = "2-eks-cluster/terraform.tfstate" # path inside the bucket
+    region         = "us-east-2"                 # AWS region
     dynamodb_table = "dev-eks-cluster-table-tf-lock"    # DynamoDB table for state locking
   }
 }
